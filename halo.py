6c30f4a4f334f59ffe2144fc5b9062123b823a37
fcseidl <fcseidl@umich.edu>
2023-01-16 15:59:48 -0700

    h = dist with perlin rotation

--- halo.py
+++ halo.py
@@ -5,24 +5,29 @@ from perlin_noise import PerlinNoise
 
 
 class Halo:
+    """
+    TODO: docstring!
+    """
 
-    def __init__(self, shape: np.ndarray, tightness=1, noise_scale=1e-2, noise_octaves=5, noise_seed=0):
+    def __init__(self, shape: np.ndarray, tightness=10, noise_octaves=0.2, noise_seed=0):
         # store real and imaginary parts separately to comply with NearestNeighbors
         self._real_shape = np.array([shape.real, shape.imag]).T
         self._nearest = NearestNeighbors(n_neighbors=1).fit(self._real_shape)
         self._tight = tightness
-        self._ns = noise_scale
-        #self._re_noise = PerlinNoise(octaves=noise_octaves, seed=noise_seed)
-        #self._im_noise = PerlinNoise(octaves=noise_octaves, seed=noise_seed + 1)
+        self._noise = PerlinNoise(octaves=noise_octaves, seed=noise_seed)
 
     def __call__(self, z: np.ndarray) -> np.ndarray:
+        # determine distances from shape
         # must flatten and convert to (real, imag) to comply with NearestNeighbors
         flat_z = z.reshape(-1)
         real_flat_z = np.array([flat_z.real, flat_z.imag]).T
-        neigh_ind = self._nearest.kneighbors(real_flat_z, return_distance=False)[:, 0]
-        neigh = self._real_shape[neigh_ind]
-        real_flat_h = real_flat_z - neigh
-        flat_h = real_flat_h[:, 0] + real_flat_h[:, 1] * 1j
-        return self._tight * flat_h.reshape(z.shape)
+        dist, _ = self._nearest.kneighbors(real_flat_z)
+        dist = dist[:, 0] ** 0.7    # TODO: document this exponent which shrinks interior
+        # obtain (near-)uniform locally correlated angles
+        theta = np.array([self._noise(xy) for xy in real_flat_z]) * np.pi
+        # rotate distances into complex plane to obtain halo
+        h = self._tight * dist * np.exp(theta * 1j)
+        return h.reshape(z.shape)
+
 
 
--- main.py
+++ main.py
@@ -10,13 +10,14 @@ if __name__ == '__main__':
     # example with straight line
     if 1:
         import numpy as np
+        np.random.seed(0)
 
-        n = 2
+        n = 200
         x = np.linspace(-1, 1, n, dtype=complex)
-        halo = Halo(x, tightness=0.5)
+        halo = Halo(x, tightness=2, noise_octaves=1)
 
-    fract = Fractal(k=5)
-    view = Viewer(fract, input_transform=halo, pixels_per_unit=200)
+    fract = Fractal(k=3)#, julia_param=-.618)
+    view = Viewer(fract, input_transform=halo, pixels_per_unit=100)
     img = view.image()
     plt.imshow(img)
     plt.axis('off')
