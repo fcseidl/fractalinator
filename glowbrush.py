3fe67ccbd1a61221b895159ec33236d0905fdbee
fcseidl <fcseidl@umich.edu>
2024-09-01 14:26:35 -0700

    oo structure introduced

--- glowbrush.py
+++ glowbrush.py
@@ -18,36 +18,49 @@ def np2image(arr):
     return ImageTk.PhotoImage(image=Image.fromarray(arr))
 
 
-# create blur of light intensity around source
-blur_width = 2 * fade_dist + 1
-blur = np.ones((blur_width, blur_width)) * brightness
-for u in range(blur_width):
-    for v in range(blur_width):
-        blur[u, v] /= ((u - fade_dist)**2 + (v - fade_dist)**2) + 1   # Laplace smoothing
+class GlowBrush:
+
+    def __init__(self, canvas, buffer, filter):
+        self.buffer = buffer
+        self.filter = filter
+        self.canvas = canvas
+
+        # create blur of light intensity around source
+        blur_width = 2 * buffer + 1
+        self.blur = np.ones((blur_width, blur_width)) * brightness
+        for u in range(blur_width):
+            for v in range(blur_width):
+                self.blur[u, v] /= ((u - buffer)**2 + (v - buffer)**2) + 1   # Laplace smoothing
+
+        self.buffered_intensity = np.zeros((height + 2 * fade_dist, width + 2 * fade_dist))
+
+        self.canvas.bind("<B1-Motion>", self.stroke)
+        self.canvas.pack(expand="yes", fill="both")
+
+        self.img = None
+
+    def stroke(self, event):
+        y, x = event.x, event.y
+        self.buffered_intensity[x:x + 2 * self.buffer + 1, y:y + 2 * self.buffer + 1] += self.blur
+        intensity = self.buffered_intensity[self.buffer:-self.buffer, self.buffer:-self.buffer]
+        arr = self.filter(intensity)
+        self.img = np2image(arr)
+        self.canvas.create_image(0, 0, anchor="nw", image=self.img)
 
 
-root = tk.Tk()
-buffered_intensity = np.zeros((height + 2 * fade_dist, width + 2 * fade_dist))
-canvas = tk.Canvas(root, width=width, height=height)
-img = None
+def basic_filter(raw):
+    return np.minimum(np.sqrt(raw), 1) * 255
 
 
-def brush(event):
-    global img
-    y, x = event.x, event.y
-    buffered_intensity[x:x + 2 * fade_dist + 1, y:y + 2 * fade_dist + 1] += blur
-    intensity = buffered_intensity[fade_dist:-fade_dist, fade_dist:-fade_dist]
-    arr = (np.minimum(np.sqrt(intensity), 1) * 255)
-    img = np2image(arr)
-    canvas.create_image(0, 0, anchor="nw", image=img)
+def main():
+    root = tk.Tk()
+    canvas = tk.Canvas(root, width=640, height=480)
+
+    GlowBrush(canvas, 200, basic_filter)
+    root.mainloop()
 
 
-canvas.bind("<B1-Motion>", brush)
-canvas.pack(expand="yes", fill="both")
-
-root.mainloop()
+if __name__ == "__main__":
+    main()
 
 
-"""import matplotlib.pyplot as plt
-plt.imshow(blur)
-plt.show()"""
