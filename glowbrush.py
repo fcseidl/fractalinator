ca0bab045ed807489177340ed4ee39b1b7290d79
fcseidl <fcseidl@umich.edu>
2024-09-01 16:45:50 -0700

    interesting bug in wheel demo

--- glowbrush.py
+++ glowbrush.py
@@ -7,37 +7,31 @@ import numpy as np
 from PIL import Image, ImageTk
 
 
-# widget parameters
-width = 640
-height = 480
-brightness = 10
-fade_dist = 300
-
-
 def np2image(arr):
-    return ImageTk.PhotoImage(image=Image.fromarray(arr))
+    return ImageTk.PhotoImage(
+        image=Image.fromarray(
+            arr.astype(
+                np.uint8)))
 
 
 class GlowBrush:
 
-    def __init__(self, canvas, buffer, filter):
+    def __init__(self, root, w, h, buffer, filter):
+        self.canvas = tk.Canvas(root, width=w, height=h)
+        self.canvas.bind("<B1-Motion>", self.stroke)
+        self.canvas.pack()
+
+        self.buffered_intensity = np.zeros((h + 2 * buffer, w + 2 * buffer))    # transposed from tk
         self.buffer = buffer
         self.filter = filter
-        self.canvas = canvas
+        self.img = None
 
-        # create blur of light intensity around source
+        # define blur of intensity around brushstrokes
         blur_width = 2 * buffer + 1
-        self.blur = np.ones((blur_width, blur_width)) * brightness
+        self.blur = np.ones((blur_width, blur_width))
         for u in range(blur_width):
             for v in range(blur_width):
-                self.blur[u, v] /= ((u - buffer)**2 + (v - buffer)**2) + 1   # Laplace smoothing
-
-        self.buffered_intensity = np.zeros((height + 2 * fade_dist, width + 2 * fade_dist))
-
-        self.canvas.bind("<B1-Motion>", self.stroke)
-        self.canvas.pack(expand="yes", fill="both")
-
-        self.img = None
+                self.blur[u, v] /= ((u - buffer) ** 2 + (v - buffer) ** 2) + 1  # Laplace smoothing
 
     def stroke(self, event):
         y, x = event.x, event.y
@@ -54,9 +48,7 @@ def basic_filter(raw):
 
 def main():
     root = tk.Tk()
-    canvas = tk.Canvas(root, width=640, height=480)
-
-    GlowBrush(canvas, 200, basic_filter)
+    GlowBrush(root, 640, 480, 200, basic_filter)
     root.mainloop()
 
 
---
+++ wheeldemo.py
@@ -0,0 +1,45 @@
+import tkinter as tk
+import numpy as np
+from matplotlib.colors import hsv_to_rgb
+from perlin_noise import PerlinNoise
+
+from glowbrush import GlowBrush
+
+
+width = 400
+height = 400
+
+
+def colorwheel(z):
+    """Convert from complex plane into rgb space according to color wheel centered at zero."""
+    z = z.astype(complex)
+    h = np.arctan2(z.real, z.imag) + np.pi
+    h /= 2 * np.pi
+    s = np.tanh(z.real**2 + z.imag**2)
+    v = np.ones_like(h)
+    hsv = np.dstack((h, s, v))
+    return hsv_to_rgb(hsv) * 255
+
+
+# obtain random smooth field of complex units
+norm = (width + height) / 10
+rnoise = PerlinNoise(octaves=1, seed=0)
+inoise = PerlinNoise(octaves=1, seed=1)
+x = np.arange(width) / norm + 20
+y = np.arange(height) / norm + 20
+x, y = np.meshgrid(x, y)
+x, y = x.reshape(-1), y.reshape(-1)
+theta = np.array([
+    np.arctan2(rnoise([xx, yy]), inoise([xx, yy]))
+    for xx, yy in zip(x, y)
+]).reshape(height, width)
+u = np.exp(1j * theta)
+
+
+def filter(z):
+    z *= theta
+    return colorwheel(z)
+
+root = tk.Tk()
+GlowBrush(root, w=width, h=height, buffer=100, filter=filter)
+root.mainloop()
