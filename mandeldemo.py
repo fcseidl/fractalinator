90808a3c44da54b734854f4a73a13c07550bf04e
fcseidl <fcseidl@umich.edu>
2024-09-17 10:45:43 -0700

    removed overflow error

--- mandeldemo.py
+++ mandeldemo.py
@@ -1,8 +1,11 @@
 import numpy as np
 from artwork import Artwork
+from matplotlib import colormaps
 
 
 niter = 80
+cmap_name = 'hsv'
+
 
 def paint(z0):
     # compute escape time map
@@ -12,12 +15,12 @@ def paint(z0):
     for it in range(0, niter):
         azn = np.abs(zn)
         escaped = (azn > 2.) & (smooth == 0.)
-        smooth[escaped] = it + np.exp(2 - azn[escaped])
-        zn[escaped] = 0.  # avoids overflow
+        smooth[escaped] = it + np.exp(2 - azn[escaped]) + 1e-8  # avoid sentinel value zero
+        zn[smooth > 0.] = 0.  # avoids overflow
         zn = fun(zn)
     # assign colors to lemniscates
-    greyscale = 255 * (smooth / niter) ** 0.5
-    return np.dstack((greyscale, greyscale, greyscale))
+    greyscale = (((smooth / niter) ** 0.3) * 4) % 1.
+    return 255 * colormaps[cmap_name](greyscale)[:, :, :3]
 
 
 w, h = (1000, 1000)
@@ -25,7 +28,7 @@ w, h = (1000, 1000)
 Artwork(
     width=w,
     height=h,
-    brush_strength=60,
+    brush_strength=50,
     buffer=100,
     paint=paint,
     noise_sig=30,
