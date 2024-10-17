4be72b17352e597d24f481e6cc65af70817316fc
fcseidl <fcseidl@umich.edu>
2024-10-16 20:00:58 -0700

    removed old modules

---
+++ demo.py
@@ -0,0 +1,7 @@
+
+from artwork import Artwork
+Artwork(720, 576,
+        brush_strength=45,
+        noise_seed=1)
+
+# it's that easy
--- mandeldemo.py
+++
@@ -1,5 +0,0 @@
-
-from artwork import Artwork
-
-Artwork(800, 600,
-        cmap_name="twilight")
