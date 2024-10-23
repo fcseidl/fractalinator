844c35b5548359e1bd8b345bd770ec4529a6249c
fcseidl <fcseidl@umich.edu>
2024-10-22 17:47:28 -0700

    restructured repo

---
+++ fractalinator/fractalinator.py
@@ -0,0 +1,15 @@
+
+from ast import literal_eval
+from sys import argv
+from artwork import Artwork
+
+
+if __name__ == "__main__":
+    kwargs = {}
+    for arg in argv[2:]:
+        try:
+            k, v = arg.split('=')
+            kwargs[k] = literal_eval(v)
+        except ValueError:
+            raise ValueError("Unable to parse keyword argument %s" % arg)
+    Artwork(**kwargs)
