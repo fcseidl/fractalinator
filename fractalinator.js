6eec08ec416a453358fb533b1833dfc508146b18
fcseidl <fcseidl@umich.edu>
2024-11-21 11:33:50 -0800

    settings dont update for unchanged pixels

--- fractalinator.js
+++ fractalinator.js
@@ -6,7 +6,7 @@ canvas.height = window.innerHeight - 30
 let ctx = canvas.getContext('2d');
 
 // Initialize artwork and paint to canvas
-async function main(params) {
+async function main() {
   let pyodide = await loadPyodide({
     packages: ["numpy", "matplotlib"]
   });
@@ -28,8 +28,7 @@ pyodideReadyPromise = main();
 
 // Track mouse to only draw when left button is down
 let drawing = false;
-
-document.addEventListener("mousedown", (event) => {
+canvas.addEventListener("mousedown", (event) => {
   if (event.button === 0) { 
     drawing = true; 
   }
@@ -40,23 +39,48 @@ document.addEventListener("mouseup", (event) => {
   } 
 });
 
+
+function clamp(value, lo, hi) {
+    if (value < lo) return lo;
+    if (value > hi) return hi;
+    return value;
+}
+
 // recalculate frame when mouse is moved
 canvas.addEventListener("mousemove", async (event) => {
   if (drawing) {
-    let pyodide = await pyodideReadyPromise;
+    // get motion location
     const rect = canvas.getBoundingClientRect();
-    const x = event.clientX - rect.left;
-    const y = event.clientY - rect.top;
-    
-    let proxy = pyodide.runPython(`
+    let x = Math.round(event.clientX - rect.left);
+    let y = Math.round(event.clientY - rect.top);
+    x = clamp(x, 0, canvas.width);
+    y = clamp(y, 0, canvas.height);
+
+    // call Python to modify image
+    let pyodide = await pyodideReadyPromise;
+    let raw = pyodide.runPython(`
       rgb = art.paint_stroke(${x}, ${y})
       createUint8ClampedArray(rgb)
-    `)
+    `).toJs();
     
-    let pixelData = new Uint8ClampedArray(proxy.toJs());
-    let brush_radius = (Math.sqrt(pixelData.length / 4) - 1) / 2
-
-    imageData = new ImageData(pixelData, 2 * brush_radius + 1)
+    // update canvas
+    let pixelData = new Uint8ClampedArray(raw);
+    let brush_radius = (Math.sqrt(pixelData.length / 4) - 1) / 2;
+    imageData = new ImageData(pixelData, 2 * brush_radius + 1);
     ctx.putImageData(imageData, x - brush_radius, y - brush_radius);
   }
 });
+
+
+// apply new kwargs when apply button is pressed
+async function onApplyClick() {
+    let pyodide = await pyodideReadyPromise;
+    pyodide.runPython(`
+        new_art = Artwork(shape=(${canvas.width}, ${canvas.height}), ${kwargs.value})
+        new_art.buffered_intensity = art.buffered_intensity
+        art = new_art    
+    `)
+
+}
+document.getElementById("apply-button").setAttribute("onclick", "onApplyClick()");
+
