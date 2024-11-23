37aa91d89c79af89ffdf3f7c0f8fb4cc0acb8433
fcseidl <fcseidl@umich.edu>
2024-11-22 22:12:54 -0800

    save images from menu

--- fractalinator.js
+++ fractalinator.js
@@ -10,8 +10,8 @@ menu.addEventListener('mouseleave', () => {
 
 // Get canvas and context elements
 let canvas = document.getElementById('canvas');
-canvas.width = window.innerWidth
-canvas.height = window.innerHeight
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
 let ctx = canvas.getContext('2d');
 
 // set up Python
@@ -32,7 +32,7 @@ async function updateCanvas() {
     let pixelData = new Uint8ClampedArray(
         pyodide.runPython(`createUint8ClampedArray(art.rgb)`).toJs()
     );
-    imageData = new ImageData(pixelData, canvas.width);
+    let imageData = new ImageData(pixelData, canvas.width);
     ctx.putImageData(imageData, 0, 0);
 }
 
@@ -99,7 +99,7 @@ canvas.addEventListener("mousemove", async (event) => {
     // update only a segment of the canvas
     let pixelData = new Uint8ClampedArray(raw);
     let brush_radius = (Math.sqrt(pixelData.length / 4) - 1) / 2;
-    imageData = new ImageData(pixelData, 2 * brush_radius + 1);
+    let imageData = new ImageData(pixelData, 2 * brush_radius + 1);
     ctx.putImageData(imageData, x - brush_radius, y - brush_radius);
   }
 });
@@ -110,10 +110,49 @@ async function onApplyClick() {
     pyodide.runPython(`art = Artwork(${getMenuArgs()}intensity=art.intensity)`);
     updateCanvas();
 }
-document.getElementById("apply-button").setAttribute("onclick", "onApplyClick()");
+document.getElementById("apply-button").addEventListener("click", onApplyClick);
 document.addEventListener("keyup", event => {
   if ((event.key == "Enter") && (menu.style.display != "none")) {
     menu.style.display = "none";
     onApplyClick();
   }
+});
+
+// bind save buttons
+async function saveArt(sf) {
+  // call Python to get high res image
+  let pyodide = await pyodideReadyPromise;
+  let raw = pyodide.runPython(`
+    rgb = art.high_res(${sf})
+    h, w, _ = rgb.shape
+    createUint8ClampedArray(rgb)
+  `).toJs();
+
+  // create temp canvas
+  let saveCanvas = document.createElement("canvas");
+  saveCanvas.width = pyodide.runPython(`w`);
+  saveCanvas.height = pyodide.runPython(`h`);
+  const saveCtx = saveCanvas.getContext("2d");
+
+  // save image to save-canvas
+  let pixelData = new Uint8ClampedArray(raw);
+  let imageData = new ImageData(pixelData, saveCanvas.width);
+  saveCtx.putImageData(imageData, 0, 0);
+
+  // perform download with temp link
+  const saveLink = document.createElement("a");
+  saveLink.href = saveCanvas.toDataURL();
+  saveLink.download = "fractalination.png";
+  saveLink.click();
+  saveLink.remove();
+  saveCanvas.remove();
+}
+document.getElementById("save-1").addEventListener("click", () => {
+  saveArt(1);
+});
+document.getElementById("save-2").addEventListener("click", () => {
+  saveArt(2);
+});
+document.getElementById("save-3").addEventListener("click", () => {
+  saveArt(3);
 });
